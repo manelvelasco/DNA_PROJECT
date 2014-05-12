159eb6e4b8dbdf3aa7c570cf0f7945dfc8aab6e9
Manel Velasco <manel.velasco@gmail.com>
2014-05-12 16:31:57 +0200

    voroni

--- ADN_1.py
+++ ADN_1.py
@@ -8,12 +8,33 @@ This temporary script file is located here:
 import random
 r=random.random
 
+class plasma:
+    def __init__(self,ENV):
+        self.ENV=ENV
+
+    def test_type(self,obj,typ):
+        if obj[0]==typ:
+            return 1
+
+    def Find_in_ENV(self,name):
+        return filter((lambda obj:self.test_type(obj,name)),self.ENV)
+
+    def Add_to_ENV(self,name,obj):
+        self.ENV.append([name,obj])
+
+    def Get_from_ENV(self,name):
+        L=self.Find_in_ENV(name)
+        if len(L)>0:
+            self.ENV.remove(L[0])
+            return L[0][1]
         
-class cell:
+class cell(plasma):
     def __init__(self,ENV,position=[0,0]):
         self.ADN=ENV[0][1]
         self.ENV=ENV
         self.position=position
+        self.BODY=None
+
     def cumulative_prob_sum(self):
         cum_sum=[]
         y=0;
@@ -28,16 +49,6 @@ class cell:
         ind=[i for i in range(len(cumulative)) if cumulative[i] > rn]
         #This executes the magic function
         self.ADN[ind[0]][1](self)
-        
-    def test_type(self,obj,typ):
-        if obj[0]==typ:
-            return 1
-        
-    def Find_in_ENV(self,name):
-        return filter((lambda obj:self.test_type(obj,name)),self.ENV)
-
-    def Find_in_ADN(self,name):
-        return filter((lambda obj:self.test_type(obj,name)),self.ADN)
 
     def change_ADN_prob(self,name, newProb):
         if newProb<=1.0 and newProb>=0.0:
@@ -56,81 +67,19 @@ class cell:
             return obj[2]
         except:
             return obj
-    def Add_to_ENV(self,name,obj):
-        self.ENV.append([name,obj])
 
-    def Get_from_ENV(self,name):
-        L=self.Find_in_ENV(name)
-        if len(L)>0:
-            self.ENV.remove(L[0])
-            return L[0][1]
+    def Find_in_ADN(self,name):
+        return filter((lambda obj:self.test_type(obj,name)),self.ADN)
+
+    
+
+class body(plasma):
+    def __init__(self,ENV,CELLS):
+        self.ENV=ENV
+        self.CELLS=CELLS
+        for icell in CELLS:
+            icell.BODY=self
 
 
 if __name__=="__main__":
-    #Example of isolated execution
-    #This example shows how to compute the 
-    #explonential of a number in random way
-    #To do so we use the taylor aproximation of exp function
-    #exp(x)=1+x+1/2! x*x + 1/3! x*x*x etc..
-    
-    #As you can see the computation of coefs and powers are unrelated!!!!
-    
-    #DNA Functions
-    def Compute_next_power(icell):
-        try:
-            max_pow=icell.Get_from_ENV('power')
-            ep=icell.Get_from_ENV('evaluated_point')
-            icell.Add_to_ENV('power',[max_pow[1]*ep,max_pow[1]+1])
-            ep=icell.Add_to_ENV('evaluated_point',ep)
-        except:
-            print "E1"
-
-    def Compute_next_coef(icell):
-        try:
-            coef=icell.Get_from_ENV('coef')
-            icell.Add_to_ENV('coef',[coef[0]/(coef[1]+1),coef[1]+1])
-        except:
-            print "E2"
-        
-    def compute_next_value(icell):
-        try:
-            coef=icell.Get_from_ENV('coef')
-            power=icell.Get_from_ENV('power')
-            value=icell.Get_from_ENV('value')
-            if value[1]==coef[1]-1 and value[1]==power[1]-1:
-                print [value[0]+coef[0]*power[0],value[1]+1]
-                icell.Add_to_ENV('value',[value[0]+coef[0]*power[0],value[1]+1])
-                print "Activation"
-            else:
-                icell.Add_to_ENV('value',value)
-            icell.Add_to_ENV('power',coef)
-            icell.Add_to_ENV('coef',power)
-        except:
-            print "E3"
-
-    def start_computation(icell):
-        icell.Add_to_ENV('power',[1.0,0.0])
-        icell.Add_to_ENV('coef',[1.0,0.0])
-        icell.Add_to_ENV('value',[0.0,-1.0])
-        icell.Add_to_ENV('evaluated_point',0.5)
-        icell.change_ADN_prob('Action4',0.05)
-        icell.change_ADN_prob('Action3',0.9995)
-    
-        
-    ADN=[['Action1',Compute_next_power,0.25],
-         ['Action2',Compute_next_coef,.25],
-         ['Action3',compute_next_value,0.25],
-         ['Action4',start_computation,0.25]]
-    ENV=[['ADN',ADN]]
-    
-    cell1=cell(ENV)
-    for i in range(100000):
-        #print i
-        cell1.execute_random_acction()
-        #try:
-            #print cell1.Find_in_ENV('value')[0]
-        #except:
-            #print "No values yet"#print cell1.Find_in_ENV('value')
-        
-    
-    
+    pass
--- Example1.py
+++ Example1.py
@@ -5,6 +5,8 @@ Created on Sat Oct 29 22:16:24 2011
 @author: -
 """
 from ADN_1 import cell
+import pprint
+import pylab
 #Example of isolated execution
 #This example shows how to compute the 
 #explonential of a number in random way
@@ -17,47 +19,75 @@ from ADN_1 import cell
 def Compute_next_power(icell):
     try:
         max_pow=icell.Get_from_ENV('power')
+        if max_pow==None:
+            add_to_pending_functions(icell, Compute_next_power)
+            return
         ep=icell.Get_from_ENV('evaluated_point')
-        icell.Add_to_ENV('power',[max_pow[1]*ep,max_pow[1]+1])
-        ep=icell.Add_to_ENV('evaluated_point',ep)
+        if ep==None:
+            icell.Add_to_ENV('power',max_pow)
+            return
+        icell.Add_to_ENV('power',[max_pow[0]*ep,max_pow[1]+1])
+        icell.Add_to_ENV('evaluated_point',ep)
     except:
-        print "E1"
+        raise
 
 def Compute_next_coef(icell):
     try:
         coef=icell.Get_from_ENV('coef')
+        if coef==None:
+            #add_to_pending_functions(icell, Compute_next_coef)
+            return
         icell.Add_to_ENV('coef',[coef[0]/(coef[1]+1),coef[1]+1])
     except:
-        print "E2"
+        raise
     
 def compute_next_value(icell):
     try:
         coef=icell.Get_from_ENV('coef')
+        if coef==None:
+            add_to_pending_functions(icell,compute_next_value)
+            return
         power=icell.Get_from_ENV('power')
+        if power==None:
+            icell.Add_to_ENV('coef',coef)
+            #add_to_pending_functions(icell,compute_next_value)
+            return
         value=icell.Get_from_ENV('value')
+        if value==None:
+            icell.Add_to_ENV('coef',coef)
+            icell.Add_to_ENV('power',power)
+            #add_to_pending_functions(icell,compute_next_value)
+            return
         if value[1]==coef[1]-1 and value[1]==power[1]-1:
-            print [value[0]+coef[0]*power[0],value[1]+1]
             icell.Add_to_ENV('value',[value[0]+coef[0]*power[0],value[1]+1])
-            print "Activation"
         else:
             icell.Add_to_ENV('value',value)
-            pending=icell.Get_from_ENV('pending')
-            print "Añadiendo funcion"  
-            pending.append(f)
-            icell.Add_to_ENV('pending',pending)
-        icell.Add_to_ENV('power',coef)
-        icell.Add_to_ENV('coef',power)
+            #add_to_pending_functions(icell,compute_next_value)
+        icell.Add_to_ENV('coef',coef)
+        icell.Add_to_ENV('power',power)
+        
     except:
-        print "E3"
+        raise
 
 def start_computation(icell):
-    icell.Add_to_ENV('power',[1.0,0.0])
-    icell.Add_to_ENV('coef',[1.0,0.0])
-    icell.Add_to_ENV('value',[0.0,-1.0])
-    icell.Add_to_ENV('evaluated_point',0.5)
-    icell.change_ADN_prob('Action4',0.01)
+    icell.Add_to_ENV('power',[0.5,1.0])
+    icell.Add_to_ENV('coef',[1.0,1.0])
+    icell.Add_to_ENV('value',[1.0,0.0])
+    ep=icell.Get_from_ENV('evaluated_point')
+    if ep==None:
+        icell.Add_to_ENV('evaluated_point',0.5)
+    else:
+        icell.Add_to_ENV('evaluated_point',ep)
+    icell.change_ADN_prob('Action4',0.005)
+    icell.change_ADN_prob('Action3',0.95)
 
-
+def add_to_pending_functions(icell, fun):
+    pending=icell.Get_from_ENV('pending')
+    if pending==None:
+        pending=[fun]
+    else:
+        pending.append(fun)
+        icell.Add_to_ENV('pending',pending)
 
 ADN=[['Action1',Compute_next_power,0.25],
      ['Action2',Compute_next_coef,.25],
@@ -66,17 +96,14 @@ ADN=[['Action1',Compute_next_power,0.25],
 ENV=[['ADN',ADN],['pending',[]]]
 
 cell1=cell(ENV)
-for i in range(10000):
-    print i
+for i in range(50000):
     cell1.execute_random_acction()
-    pending=cell1.Find_in_ENV('pending')
-    #print pending
-    if len(pending):
-        for el in pending[0][1]:
-            print el
-            el(cell1)
-    #try:
-        #print cell1.Find_in_ENV('value')[0]
-    #except:
-        #print "No values yet"#print cell1.Find_in_ENV('value')
-print ENV
+#    
+pp=pprint.PrettyPrinter(indent=4)
+pp.pprint(cell1.Find_in_ENV('value'))
+valores=[];
+for el in cell1.Find_in_ENV('value'):
+    valores.append(el[1][1])
+print valores
+pylab.hist(valores,bins=max(valores))
+pylab.show()
